b6940d82c7eb3d921089566d3e314885a040d1de
Andrew huggins <andrewhuggins@gmail.com>
2016-09-17 19:41:25 -0400

    update menu and composer

--- src/menu.php
+++ src/menu.php
@@ -3,3 +3,8 @@
 Menu::make('admin-main', function ($menu) {
     
 });
+
+Menu::make('admin-sidebar', function ($menu) {
+    $menu->add('Dashboard', ['url' => 'dashboard'])
+    ->prepend('<i class="fa fa-tachometer"></i>');
+});
