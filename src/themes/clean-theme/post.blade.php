c1760240e3223a67508d7bab949017b5fb2312fb
Andrew huggins <andrewhuggins@gmail.com>
2016-09-21 21:55:07 -0400

    move markdown parse to theme

--- src/themes/clean-theme/post.blade.php
+++ src/themes/clean-theme/post.blade.php
@@ -25,7 +25,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                    {!! $post->body !!}
+                    {!! Markdown::convertToHtml($post->body) !!}
                 </div>
             </div>
         </div>
